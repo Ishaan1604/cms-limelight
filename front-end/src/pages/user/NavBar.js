6e79a0dcf503e0af94cebafa631e2b7d17b5e89d
Ishaan Agarwal <IshaanAgarwal2@Ishaans-MacBook-Pro.local>
2024-05-31 16:18:58 +0530

    Finished nav-bar

--- front-end/src/pages/user/NavBar.js
+++ front-end/src/pages/user/NavBar.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <nav>
-      
+    <nav className='flex row'>
+      <ul style={{width: '20%', fontSize: 'larger'}} className='flex row center'>
+        <li>
+          <a href={`/${localStorage.name}`}>CMS</a>
+        </li>
+      </ul>
+      <ul className='flex row' style={{width: '60%', justifyContent: 'space-evenly'}}>
+        <li>
+          <a href={`/${localStorage.name}/policies`}>Policies</a>
+        </li>
+        <li>
+          <a href={`/${localStorage.name}/myPolicies`}>My Policies</a>
+        </li>
+        <li>
+          <a href={`/${localStorage.name}/claims`}>Claims</a>
+        </li>
+      </ul>
     </nav>
   )
 }
