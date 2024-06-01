970a18d02af1604400eac5ee72c05d60688ee9db
Ishaan Agarwal <IshaanAgarwal2@Ishaans-MacBook-Pro.local>
2024-06-01 19:25:02 +0530

    Admin home complete, testing left

--- front-end/src/components/Updates.js
+++ front-end/src/components/Updates.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Updates({type, content}) {
-    let message = `No ${type} updates`;
+    let message = '';
     if (content.updatedAt === content.createdAt) {
         message = `New ${type} added: ${content.policyName || content.name}`
     }
@@ -12,6 +12,10 @@ function Updates({type, content}) {
 
     if (type === 'policy' && content.active === 'false') {
         message = `Policy ${content.name} expired on ${content.updatedAt}`
+    }
+
+    if (type === 'user-policy' && content.expired === 'true') {
+        message = `Your policy ${content.policyName} expired on ${content.updatedAt}`
     }
 
     if (type === 'user-policy' && content.expired === 'true') {
--- front-end/src/pages/admin/HomeAdmin.js
+++ front-end/src/pages/admin/HomeAdmin.js
@@ -1,9 +1,105 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import NavBar from './NavBar'
+import { Updates } from '../../components';
+import axios from 'axios';
 
 function HomeAdmin() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({err: false, msg: null});
+  const [updates, setUpdates] = useState({policies: [], users: [], claims: [], lengths: {}})
+
+  const fetchData = async() => {
+    try {
+      setIsLoading(true)
+
+      // const {data: policies} = await axios.get(`http://localhost:3000/api/v1/cms/policies?sort=-updatedAt`, {
+      //   headers: {
+      //     Authorization: `Bearer ${localStorage.token}`
+      //   }
+      // })
+
+      // const {data: users} = await axios.get(`http://localhost:3000/api/v1/cms/admin/users?sort=-updatedAt`, {
+      //   headers: {
+      //     Authorization: `Bearer ${localStorage.token}`
+      //   }
+      // })
+
+      // const {data: claims} = await axios.get(`http://localhost:3000/api/v1/cms/admin/claims?sort=-updatedAt`, {
+      //   headers: {
+      //     Authorization: `Bearer ${localStorage.token}`
+      //   }
+      // })
+
+      // setUpdates({policies: [policies[0], policies[1], policies[2]], users: [users[0], users[1], users[2]], claims: [claims[0], claims[1], claims[2]], lengths: {policies: policies.length, users: users.length, claims: claims.length}})
+    } catch (error) {
+      setError({err: true, msg: error.message})
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <p></p>
+      </div>
+    )
+  }
+  if (error.err) {
+    return (
+      <div className='error-div'>
+        <p>{error.msg}</p>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      
+    <div className='flex row'>
+      <NavBar/>
+      <div className='side-bar flex column center'>
+        <h1>Welcome!</h1>
+        <h1 style={{textAlign: 'center'}}>{localStorage.name}</h1>
+      </div>
+      <div className='home-container grid'>
+        <div className='home-tile flex row' style={{ justifyContent: 'space-evenly'}}>
+          <h1>General Info</h1>
+          <h1>{`Number of users: ${updates.lengths.users || 0}`}</h1>
+          <h1>{`Number of policies: ${updates.lengths.policies || 0}`}</h1>
+          <h1>{`Number of claims: ${updates.lengths.claims || 0}`}</h1>
+        </div>
+        <div className='home-tile flex row' style={{ justifyContent: 'space-evenly'}}>
+          <h1>Policy Updates</h1>
+          {
+            updates.policies.length > 0 ? updates.policies.map((policy) => {
+              return <Updates type='policy' content={policy} />
+            }) : <h1>No Policy Updates</h1>
+          }
+        </div>
+        <div className='home-tile flex row' style={{ justifyContent: 'space-evenly'}}>
+          <h1>User Updates</h1>
+          {
+            updates.users.length > 0 ? updates.users.map((user) => {
+              return <Updates type='user' content={user} />
+            }) : <h1>No User Updates</h1>
+          }
+        </div>
+        <div className='home-tile flex row' style={{ justifyContent: 'space-evenly'}}>
+          <h1>Claims Update</h1>
+          {
+            updates.claims.length > 0 ? updates.claims.map((claim) => {
+              return <Updates type='claim' content={claim} />
+            }) : <h1>No Claim Updates</h1>
+          }
+        </div>
+      </div>
     </div>
   )
 }
